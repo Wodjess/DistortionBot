ba97edb3dea728a8fbfb21fced30edc7fedb7617
Wodjess <74964006+Wodjess@users.noreply.github.com>
2023-02-17 23:10:15 +0300

    Main

---
+++ gzmyxbot.py
@@ -0,0 +1,99 @@
+﻿
+from tkinter.tix import InputOnly
+from aiogram import Bot, Dispatcher, executor, types
+from skimage import img_as_ubyte
+from skimage.io import imread, imsave
+from block_distortion import distort_image
+import numpy as np
+from PIL import Image
+lhs = '6280020623:AAFzkWwKR-fYdsE0ktXcxSTCyyjEpwXRZx8'
+bot = Bot(token=lhs)
+dp = Dispatcher(bot)
+@dp.message_handler()
+
+def quad_as_rect(quad):
+    if quad[0] != quad[2]: return False
+    if quad[1] != quad[7]: return False
+    if quad[4] != quad[6]: return False
+    if quad[3] != quad[5]: return False
+    return True
+
+def quad_to_rect(quad):
+    assert(len(quad) == 8)
+    assert(quad_as_rect(quad))
+    return (quad[0], quad[1], quad[4], quad[3])
+
+def rect_to_quad(rect):
+    assert(len(rect) == 4)
+    return (rect[0], rect[1], rect[0], rect[3], rect[2], rect[3], rect[2], rect[1])
+
+def shape_to_rect(shape):
+    assert(len(shape) == 2)
+    return (0, 0, shape[0], shape[1])
+
+def griddify(rect, w_div, h_div):
+    w = rect[2] - rect[0]
+    h = rect[3] - rect[1]
+    x_step = w / float(w_div)
+    y_step = h / float(h_div)
+    y = rect[1]
+    grid_vertex_matrix = []
+    for _ in range(h_div + 1):
+        grid_vertex_matrix.append([])
+        x = rect[0]
+        for _ in range(w_div + 1):
+            grid_vertex_matrix[-1].append([int(x), int(y)])
+            x += x_step
+        y += y_step
+    grid = np.array(grid_vertex_matrix)
+    return grid
+
+def distort_grid(org_grid, max_shift):
+    new_grid = np.copy(org_grid)
+    x_min = np.min(new_grid[:, :, 0])
+    y_min = np.min(new_grid[:, :, 1])
+    x_max = np.max(new_grid[:, :, 0])
+    y_max = np.max(new_grid[:, :, 1])
+    new_grid += np.random.randint(- max_shift, max_shift + 1, new_grid.shape)
+    new_grid[:, :, 0] = np.maximum(x_min, new_grid[:, :, 0])
+    new_grid[:, :, 1] = np.maximum(y_min, new_grid[:, :, 1])
+    new_grid[:, :, 0] = np.minimum(x_max, new_grid[:, :, 0])
+    new_grid[:, :, 1] = np.minimum(y_max, new_grid[:, :, 1])
+    return new_grid
+
+def grid_to_mesh(src_grid, dst_grid):
+    assert(src_grid.shape == dst_grid.shape)
+    mesh = []
+    for i in range(src_grid.shape[0] - 1):
+        for j in range(src_grid.shape[1] - 1):
+            src_quad = [src_grid[i    , j    , 0], src_grid[i    , j    , 1],
+                        src_grid[i + 1, j    , 0], src_grid[i + 1, j    , 1],
+                        src_grid[i + 1, j + 1, 0], src_grid[i + 1, j + 1, 1],
+                        src_grid[i    , j + 1, 0], src_grid[i    , j + 1, 1]]
+            dst_quad = [dst_grid[i    , j    , 0], dst_grid[i    , j    , 1],
+                        dst_grid[i + 1, j    , 0], dst_grid[i + 1, j    , 1],
+                        dst_grid[i + 1, j + 1, 0], dst_grid[i + 1, j + 1, 1],
+                        dst_grid[i    , j + 1, 0], dst_grid[i    , j + 1, 1]]
+            dst_rect = quad_to_rect(dst_quad)
+            mesh.append([dst_rect, src_quad])
+    return mesh
+
+@dp.message_handler(content_types=['photo'])
+async def get_photo(message: types.Message):
+    global bot
+    await message.photo[-1].download(destination_file='A:\YN\gzmyxbot\gzmyxbot\lshk.jpg')
+    im = Image.open('A:\YN\gzmyxbot\gzmyxbot\lshk.jpg')
+    dst_grid = griddify(shape_to_rect(im.size), 4, 4)
+    src_grid = distort_grid(dst_grid, 50)
+    mesh = grid_to_mesh(src_grid, dst_grid)
+    im = im.transform(im.size, Image.MESH, mesh)
+    input_image = imread('lshk.jpg')
+    gzmyx = distort_image(input_image)
+    imsave("./gzmyx-gotovo.png", img_as_ubyte(gzmyx))
+    im.save("A:\YN\gzmyxbot\gzmyxbot\gzmyx-gotovo.png")
+    photo = open('gzmyx-gotovo.png', 'rb')
+    
+    await bot.send_photo(chat_id=message.chat.id, photo=photo)
+
+if __name__ == '__main__':
+  executor.start_polling(dp)
